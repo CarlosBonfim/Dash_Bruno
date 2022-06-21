0575d749d993431862504c18ea52bf415a57d49b
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2022-06-21 16:01:29 -0400

    Foi alterado o menu responsivo para um dropdown

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,24 @@
 var el = document.querySelector(".menu-mobile-icon");
 
+// el.addEventListener("click", () => {
+//     //mostrar o menu
+//     var menuItems = document.querySelector(".menu-items");
+//     if (menuItems.classList.contains("show")) {
+//         menuItems.classList.add("hidde");
+//         menuItems.classList.remove("show");
+//     } else {
+//         menuItems.classList.add("show");
+//         menuItems.classList.remove("hidde");
+//     }
+// });
+
+// function myFunction() {
+//     console.log('Teste pra saber se entrou ');
+// }
+
 el.addEventListener("click", () => {
-    //mostrar o menu
+    console.log('entrou no evento');
     var menuItems = document.querySelector(".menu-items");
-    if (menuItems.classList.contains("show")) {
-        menuItems.classList.add("hidde");
-        menuItems.classList.remove("show");
-    } else {
-        menuItems.classList.add("show");
-        menuItems.classList.remove("hidde");
-    }
+    menuItems.classList.toggle("show");
+
 });
